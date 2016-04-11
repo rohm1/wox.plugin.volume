5e7ad3fe78bae71139365335824e108aa2c2c1bb
Romain Perez <romain.perez@check24.de>
2016-04-12 00:35:22 +0200

    JSON config

---
+++ Wox.Plugin.Volume/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace Wox.Plugin.Volume
+{
+    class Config
+    {
+        public int step { get; set; }
+        public string up { get; set; }
+        public string down { get; set; }
+    }
+}
--- Wox.Plugin.Volume/Main.cs
+++ Wox.Plugin.Volume/Main.cs
@@ -1,75 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Web.Script.Serialization;
 using CoreAudioApi;
+using System.IO;
 
 namespace Wox.Plugin.Volume
 {
     public class Main : IPlugin
     {
-        const int VOLUME_STEP = 5;
+        private PluginInitContext context;
         private MMDevice PlaybackDevice;
+        private Config config;
         private bool initialized = false;
 
         public void Init(PluginInitContext context)
         {
+            this.context = context;
+
+            try
+            {
+                string json = (new StreamReader(context.CurrentPluginMetadata.PluginDirectory + "\\config.json")).ReadToEnd();
+                config = (new JavaScriptSerializer()).Deserialize<Config>(json);
+            }
+            catch (System.Exception)
+            {
+                return;
+            }
+
             try
             {
                 PlaybackDevice = MMDeviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-                initialized = true;
-            } catch (CoreAudioException e)
-            {
             }
+            catch (CoreAudioException)
+            {
+                return;
+            }
+
+            initialized = true;
         }
 
         public List<Result> Query(Query query)
         {
             List<Result> results = new List<Result>();
-            addResult(results, query);
+            results.Add(GetResult(query));
             return results;
         }
 
-        private void addResult(List<Result> results, Query query)
+        private Result GetResult(Query query)
         {
+            Result result = new Result()
+            {
+                Title = "Current volume",
+                SubTitle = "no info",
+                IcoPath = "Images\\icon.png"
+            };
+
             if (!initialized)
             {
-                results.Add(new Result()
-                {
-                    Title = "Current volume",
-                    SubTitle = "no info",
-                    IcoPath = "Images\\icon.png"
-                });
-                return;
+                return result;
             }
 
             string queryString = query.ToString();
             string lastChar = queryString.Substring(queryString.Length - 1);
 
             int volume = GetVolume();
-            if (lastChar.Equals("+"))
+            if (lastChar.Equals(config.up))
             {
-                volume += VOLUME_STEP;
+                volume += config.step;
             }
-            else if (lastChar.Equals("-"))
+            else if (lastChar.Equals(config.down))
             {
-                volume -= VOLUME_STEP;
+                volume -= config.step;
             }
 
             SetVolume(volume);
             volume = GetVolume();
 
             string volumeDesc = "[";
-            for (int i = 0; i < 100; i += VOLUME_STEP)
+            for (int i = 0; i < 100; i += config.step)
             {
                 volumeDesc = string.Concat(volumeDesc, volume > i ? "=" : "-");
             }
             volumeDesc = string.Concat(volumeDesc, "]");
 
-            results.Add(new Result()
-            {
-                Title = "Current volume",
-                SubTitle = volumeDesc,
-                IcoPath = "Images\\icon.png"
-            });
+            result.SubTitle = volumeDesc;
+            return result;
         }
 
         public int GetVolume()
